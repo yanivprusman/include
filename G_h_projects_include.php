282d74891b62f02f0497935ddce0a073be9c04a6
Yaniv <yanivprusman3@gmail.com>
2024-04-25 12:38:22 +0300

    vendor name change

--- G_h_projects_include.php
+++ G_h_projects_include.php
@@ -1,6 +1,6 @@
 <?php
 declare (strict_types=1);
-namespace G_H_PROJECTS_INCLUDE;
+namespace Include;
 // define('GHProjectsDir',"D:/125_xampp/htdocs/include/"); 
 use G_H_PROJECTS_INCLUDE\G_h_projects_functions;
 use G_H_PROJECTS_INCLUDE\Html;
